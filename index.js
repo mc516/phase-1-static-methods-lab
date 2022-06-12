358d74f7471dbdf7d8e7cea761f09791e5dd203d
Mickey <mickeyc11111@gmail.com>
2022-06-12 10:49:22 -0400

    commit tree 687a9aafc18df32fae5809c72007c4341c619d13

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(string){
+    let words = string.toLowerCase().split(" ");
+    let filter = words.map(element => {
+      if(element === "the" || element === "a" || element === "an" || element === "but" || element === "of" || element === "and" || element === "for" || element === "at" || element === "by" || element === "from"){
+        return element;
+      } else {
+        return this.capitalize(element)
+      }
+    })
+    let joinedFilter = filter.join(" ").slice(1);
+    return this.capitalize(string[0]) + joinedFilter;
+}
+
 }
